734072fe2c2ffdcefbce2fee24fcd4e3c25c6956
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-27 18:48:47 -0300

    Initial commit

---
+++ Atividade1/Models/Pessoa.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade1.Models
+{
+    public class Carro
+    {
+        private string _modelo;
+        private string _versao;
+        private int _ano;
+        public string Modelo 
+        { 
+            get
+            {
+                return _modelo.ToUpper();
+            } 
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O modelo não pode ser vazio!");
+                }
+                _modelo = value;
+            }
+        }
+        public string Versao { get; set; }
+
+        public string ModeloVersao => $"{Modelo} {Versao}";
+        public int Ano 
+        { 
+            get => _ano; 
+            
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("O ano não pode ser negativo!");
+                }
+                _ano = value;
+            } 
+        }
+        public void Apresentar() {
+            Console.WriteLine($"[Carro] modelo: {ModeloVersao}, Lançado em: {Ano}.");
+        }
+    }
+}
---
+++ Atividade1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Atividade1.Models;
+
+Carro novoCarro = new Carro();
+novoCarro.Modelo = "Prisma";
+novoCarro.Versao = "1.4";
+novoCarro.Ano = 2006;
+novoCarro.Apresentar();
