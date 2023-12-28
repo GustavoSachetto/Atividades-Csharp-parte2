57915511b924ac15b8ee0e1f36994e91f0dfd582
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-28 18:07:05 -0300

    add

---
+++ Atividade3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-EU"); // Mudando a cultura do sistema (idioma / sistema monetario)
+
+decimal dinheiro = 1999.23M;
+
+Console.WriteLine($"Meu saldo é: {dinheiro:C}"); // Imprimir a variavel como valor monetario
+ 
+// Formatar a variavel para valor monetario
+Console.WriteLine(dinheiro.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+Console.WriteLine(dinheiro.ToString("C4")); // Almentando o numero de casas decimais para 4
+
+double porcentagem = .4523;
+Console.WriteLine(porcentagem.ToString("P")); // Formatando para porcentagem
+
+int cep = 97855080;
+Console.WriteLine(cep.ToString("##-###-###")); // Formatando inventando moda
+
+DateTime data = DateTime.Now;
+DateTime data1 = DateTime.Parse("29/04/2022 18:00");
+string data2 = "31/02/1980";
+
+DateTime.TryParseExact(data2, "yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data3);
+
+Console.WriteLine(data.ToShortTimeString());
+Console.WriteLine(data1.ToString("MM"));
+Console.WriteLine(data2);
+Console.WriteLine(data3);
--- Atividade3/obj/Debug/net8.0/Atividade3.AssemblyInfo.cs
+++ Atividade3/obj/Debug/net8.0/Atividade3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a77f6c416a507406f329d08425407854dede3fb")]
-[assembly: System.Reflection.AssemblyProductAttribute("Atividade1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Atividade1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Atividade3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Atividade3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
