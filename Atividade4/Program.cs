93bc755202f144669421e21b3304036ac2d69159
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-01-03 12:02:02 -0300

    add

---
+++ Atividade4/Program.cs
@@ -0,0 +1,26 @@
+﻿try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoleitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+} 
+catch (FileNotFoundException ex) // Exceção especifica
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+} 
+catch (DirectoryNotFoundException ex) // Exceção especifica
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
+} 
+catch (Exception ex) // Exceção genérica
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+finally {
+    Console.WriteLine("Chegou até aqui.");
+}
+
+throw new Exception("Ocorreu ua exceção"); // Cria uma nova exceção
